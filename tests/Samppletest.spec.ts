89a383fe2f1213eef69999173caf1c0f8f194465
Shubham Jain <100926726+shubhamvkjain@users.noreply.github.com>
2024-08-29 20:06:13 +0530

    New changes

--- tests/Samppletest.spec.ts
+++ tests/Samppletest.spec.ts
@@ -1,13 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('first test', async({page}) => {
+
   await page.goto('https://www.saucedemo.com/')
   await page.getByPlaceholder('Username').fill('standard_user')
-  await page.locator('[data-test="password"]').fill('secret_sauce')
-  await page.locator('[data-test="login-button"]').click()
-  await page.locator('[data-test="inventory-item-sauce-labs-backpack-img"]')
-  await page.locator('[id="add-to-cart-sauce-labs-backpack"]').click()
-  await page.locator('.shopping_cart_link').click()
-  await page.locator('#checkout').click()
+  await page.getByPlaceholder('Password').fill('secret_sauce')
+  await page.getByRole('button', {name: 'login'}).click()
+  await expect(page.getByText('Products')).toBeVisible()
+})
 
-});
